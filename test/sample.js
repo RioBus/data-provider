8fbe41f09a4d17d04910319c8ae4a1d945af6844
Fred Souza <fredericoamsouza@gmail.com>
2015-04-30 16:34:22 -0300

    Configuring testing environment

---
+++ test/sample.js
@@ -0,0 +1,10 @@
+let assert = require('assert');
+
+describe("Sample test", function() {
+
+    it("should be true", function(done){
+        "use strict";
+        assert.equal(true, true);
+        done();
+    });
+});
