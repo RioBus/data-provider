8ac092290235c6bc2f407880e83724e4a8ca4a80
Fred Souza <fredericoamsouza@gmail.com>
2015-11-01 00:42:39 -0200

    Combining models

--- src/model/bus.js
+++ src/model/bus.js
@@ -1,17 +1,17 @@
+const GeolocatedSpot = require('./geolocatedSpot');
 const Moment = require('moment-timezone');
 /**
  * Describes a bus instance
  * @class {Bus}
  */
-class Bus {
+class Bus extends GeolocatedSpot {
 	
 	constructor(line, order, speed, direction, latitude, longitude, timestamp, sense) {
+		super(latitude, longitude);
 		this.line = line.toString() || 'desconhecido';
 		this.order = order.toString();
 		this.speed = speed || 0;
 		this.direction = direction || 0;
-		this.latitude = latitude;
-		this.longitude = longitude;
 		this.timestamp = Moment.tz(timestamp, "America/Sao_Paulo").format();
 		this.sense = sense || 'desconhecido';
     }
--- src/model/geolocatedSpot.js
+++ src/model/geolocatedSpot.js
@@ -2,11 +2,11 @@
  * Describes one spot of the Itinerary of a given line
  * @class {ItinerarySpot}
  */
-class ItinerarySpot {
+class GeolocatedSpot {
 
     constructor(latitude, longitude) {
 		this.latitude = latitude;
 		this.longitude = longitude;
 	}
 }
-module.exports = ItinerarySpot;
+module.exports = GeolocatedSpot;
