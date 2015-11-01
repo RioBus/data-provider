a01fbf51daff7b7f705913326d369426f5a3c866
Fred Souza <fredericoamsouza@gmail.com>
2015-11-01 02:49:42 -0200

    Refactoring models

--- src/model/bus.js
+++ src/model/bus.js
@@ -1,21 +1,21 @@
 'use strict';
-const GeolocatedSpot = require('./geolocatedSpot');
+const Spot = require('./spot');
 const Moment = require('moment-timezone');
 /**
  * Describes a bus instance
  * @class {Bus}
  */
-class Bus extends GeolocatedSpot {
+class Bus extends Spot {
 	
 	constructor(line, order, speed, direction, latitude, longitude, timestamp, sense) {
 		super(latitude, longitude);
-		this.line = line.toString() || 'desconhecido';
+		this.line = (!line || line==='')? 'indefinido' : line.toString();
 		this.order = order.toString();
 		this.speed = speed || 0;
 		this.direction = direction || 0;
 		timestamp = (new Date(timestamp)).toISOString();
 		this.timestamp = Moment.tz(timestamp, "America/Sao_Paulo").format();
-		this.sense = sense || 'desconhecido';
+		this.sense =  (!sense || sense==='')? 'desconhecido' : sense;
     }
 }
 module.exports = Bus;
--- src/model/itinerary.js
+++ src/model/itinerary.js
@@ -6,8 +6,8 @@
 class Itinerary {
 	
 	constructor(line, description, agency, keywords, spots) {
-		this.line = line.toString() || 'desconhecido';
-		this.description = description || 'desconhecido';
+		this.line = (!line || line==='')? 'indefinido' : line.toString();
+		this.description = (!description || description==='')? 'desconhecido' : description;
 		this.agency = agency || '';
 		this.keywords = keywords || '';
 		this.spots = spots || [];
