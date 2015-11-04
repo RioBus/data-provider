282be2f5e566841f711062fbf101ea129daab7f9
Fred Souza <fredericoamsouza@gmail.com>
2015-11-03 23:30:51 -0200

    Fixing cache path

--- src/core/cache.js
+++ src/core/cache.js
@@ -1,22 +1,22 @@
 /* global Buffer; */
 'use strict';
-const File   = require("./file");
-const Config = require("../config");
+const File   = require('./file');
+const Config = require('../config');
 
 class Cache {
 	
 	constructor(key) {
-		var dir = Config.cachePath;
-		if(dir[dir.length-1]!=="/") dir += "/";
+		var dir = Config.cache;
+		if(dir[dir.length-1]!=='/') dir += '/';
 		this.file = new File(dir+key);
 	}
 	
 	retrieve() {
-		return new Buffer(this.file.read(), "base64").toString("utf8");
+		return new Buffer(this.file.read(), 'base64').toString('utf8');
 	}
 	
 	write(content) {
-		this.file.write(new Buffer(content).toString("base64"))
+		this.file.write(new Buffer(content).toString('base64'));
 	}
 }
 module.exports = Cache;
