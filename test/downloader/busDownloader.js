1855dbe0304d6562a2df79348cc35ea585a441a5
Fred Souza <fredericoamsouza@gmail.com>
2015-11-04 02:04:03 -0200

    Guarantees that all the buses in the downloaded array are distinct

--- test/downloader/busDownloader.js
+++ test/downloader/busDownloader.js
@@ -8,7 +8,7 @@ const Bus = require(`${base}/model/bus`);
 const BusDownloader = require(`${base}/downloader/busDownloader`);
 const Config = require(`${base}/config`);
 
-var urlREGULAR, urlBRT;
+var urlREGULAR, urlBRT, list;
 
 describe('BusDownloader', () => {
 	
@@ -24,6 +24,17 @@ describe('BusDownloader', () => {
 		Assert.notEqual(data, null);
 		Assert(data instanceof Array);
 		Assert(data[0] instanceof Bus);
+		list = data;
+		done();
+	});
+	
+	it('should have the number count of docs in the array equal to the number of orders', (done) => {
+		const distinctOrders = [];
+		for(let bus of list) {
+			if(distinctOrders.indexOf(bus.order)>-1) continue;
+			distinctOrders.push(bus.order);
+		}
+		Assert.equal(distinctOrders.length, list.length);
 		done();
 	});
 	
