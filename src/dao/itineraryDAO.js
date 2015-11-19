df2a51b30e47cab403c9e57ea0342535ce40bfc0
Fred Souza <fredericoamsouza@gmail.com>
2015-11-19 00:05:24 -0200

    Documenting ItineraryDAO class

--- src/dao/itineraryDAO.js
+++ src/dao/itineraryDAO.js
@@ -1,6 +1,9 @@
 'use strict';
 /* global database; */
-
+/**
+ * Itinerary Data Access Object
+ * @class {BusDAO}
+ */
 class ItineraryDAO {
 	
 	constructor(connection) {
@@ -8,10 +11,19 @@ class ItineraryDAO {
 		this.collection = connection.collection('itinerary');
 	}
 	
+	/**
+	 * Retrieves all itinerary data contained in the data repository
+	 * @return {Promise}
+	 */
 	getAll() {
 		return this.collection.find({});
 	}
 	
+	/**
+	 * Saves a new itinerary
+	 * @param {Itinerary} itinerary - Itinerary data to be inserted
+	 * @return {Promise}
+	 */
 	save(itinerary) {
 		return this.collection.insert(itinerary);
 	}
